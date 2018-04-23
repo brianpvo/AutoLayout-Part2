ec87f832a05786f5e62c3f62ba99190b5af9467f
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-04-22 18:49:11 -0700

    setup Autolayout

---
+++ AutoLayout-Part2/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  AutoLayout-Part2
+//
+//  Created by Brian Vo on 2018-04-21.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonTapped:(UIButton *)sender {
+    if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"X"]) {
+        [sender setTitle:@"A very long title for this button"
+                forState:UIControlStateNormal];
+    } else {
+        [sender setTitle:@"X" forState:UIControlStateNormal];
+    }
+}
+
+@end
